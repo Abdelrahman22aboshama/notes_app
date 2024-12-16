97cee135c37567c725469f2c0f8fa15a8de30a72
Abderahman22aboshama <Abdelrahman.Aboshama22@gmail.com>
2024-12-16 03:49:59 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(NotesApp());
+}
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container();
+  }
+}
